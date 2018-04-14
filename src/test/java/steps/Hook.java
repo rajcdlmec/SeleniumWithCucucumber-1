85570fcd7c29e56890347d0109d7cafe9d22e945
rajcdlmec <raj.cdlmec@gmail.com>
2018-04-14 19:50:22 +0000

    Update Hook.java
    update chromedriver location

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/home/dev/tools/chromedriver");
         base.Driver = new ChromeDriver();
     }
 
